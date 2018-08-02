28584cb93b271c8a4f7e7242701566f5983bbbfc
Shimaa Magdi <shimaa.magdi@samesystem.com>
2018-08-03 00:18:46 +0300

    Update UI

--- nkoodiVendor/nkoodiVendor/QRCode/QRReaderViewController.swift
+++ nkoodiVendor/nkoodiVendor/QRCode/QRReaderViewController.swift
@@ -9,14 +9,16 @@
 import Foundation
 import UIKit
 
-class QRReaderViewController: UIViewController {
+class QRReaderViewController: BaseViewController {
     
     @IBOutlet weak var logoImageView: UIImageView!
     @IBOutlet weak var transferContactNameLabel: UILabel!
     @IBOutlet weak var qrCodeTextField: UITextField!
     @IBOutlet weak var amountTextField: UITextField!
+    @IBOutlet weak var container1: UIView!
+    @IBOutlet weak var conatiner2: UIView!
+    @IBOutlet weak var transferButton: UIButton!
     var loading: UIActivityIndicatorView!
-    
     var qrCodeTextFieldHasChanged: ((String)->())?
     
     public static func create() -> QRReaderViewController {
@@ -28,8 +30,34 @@ class QRReaderViewController: UIViewController {
         loading = UIActivityIndicatorView(frame: self.view.bounds)
         qrCodeTextField.delegate = self
         qrCodeTextFieldHasChanged = { text in
-            self.transferContactNameLabel.text = "user name: " + text
+            TransactionsManager.shared.getUserWithQR(text, completion: { (user) in
+                guard let user = user else { return }
+                self.transferContactNameLabel.text = "Will transfer to: " + user.name
+            })
+            //self.transferContactNameLabel.text = "user name: " + text
         }
+        self.view.backgroundColor = UIColor(hexString: "#00c6c10")
+        ////
+        qrCodeTextField.borderStyle = .none
+        let option1 = NSMutableAttributedString(string: "QR code")
+        option1.addAttribute(NSForegroundColorAttributeName, value: UIColor.white, range: NSRange(location: 0, length: option1.length))
+        option1.addAttribute(NSFontAttributeName, value: UIFont.init(name: "Arial", size: 16), range: NSRange(location: 0, length: option1.length))
+        qrCodeTextField.attributedPlaceholder = option1
+        container1.layer.cornerRadius = 22
+        container1.layer.borderColor = UIColor.white.cgColor
+        container1.layer.borderWidth = 1
+        /////
+        amountTextField.borderStyle = .none
+        let option2 = NSMutableAttributedString(string: "Amount")
+        option2.addAttribute(NSForegroundColorAttributeName, value: UIColor.white, range: NSRange(location: 0, length: option2.length))
+        option2.addAttribute(NSFontAttributeName, value: UIFont.init(name: "Arial", size: 16), range: NSRange(location: 0, length: option2.length))
+        amountTextField.attributedPlaceholder = option2
+        conatiner2.layer.cornerRadius = 22
+        conatiner2.layer.borderColor = UIColor.white.cgColor
+        conatiner2.layer.borderWidth = 1
+        /////
+        transferButton.layer.cornerRadius = 22
+        
     }
     
     @IBAction func camerButtonTapped(_ sender: Any) {
@@ -42,13 +70,10 @@ class QRReaderViewController: UIViewController {
         present(vc, animated: true, completion: nil)
     }
     
-    @IBAction func contactsButtonTapped(_ sender: Any) {
-    }
-    
     @IBAction func transferButtonTapped(_ sender: Any) {
         
         if qrCodeTextField.text == "" || amountTextField.text == "" {
-            let alert = UIAlertController(title: "Alert!", message: "you should enter contact and amount", preferredStyle: UIAlertControllerStyle.alert)
+            let alert = UIAlertController(title: "Alert!", message: "You should enter QR code and amount", preferredStyle: UIAlertControllerStyle.alert)
             alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
                 switch action.style{
                 case .default:
@@ -63,14 +88,12 @@ class QRReaderViewController: UIViewController {
                 }}))
             self.present(alert, animated: true, completion: nil)
         }else {
-            //TODO: Show Loading
-            
             self.view.addSubview(loading)
             loading.startAnimating()
-          requestTransaction()
+            requestTransaction()
+            
         }
     }
-    
     func requestTransaction(){
         if let userQr = qrCodeTextField.text,
             let amountStr = amountTextField.text,
@@ -87,9 +110,15 @@ class QRReaderViewController: UIViewController {
 }
 
 extension QRReaderViewController: UITextFieldDelegate {
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+    
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         guard let text = textField.text else { return true }
         qrCodeTextFieldHasChanged?(text)
         return true
     }
 }
+
+//
+//
+
+
